1314d33e9618a19eac31bc2531ad75a3eda4e85d
Ballazi <balram17dec1999@gmail.com>
2022-03-31 19:40:56 +0530

    ReactRouterDomeSix

--- src/index.js
+++ src/index.js
@@ -1,17 +1,118 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter, Link, Navigate, NavLink, Outlet, Route, Routes, useLocation, useNavigate, useParams } from 'react-router-dom';
+// import './index.css';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/myApp" element={<Navigate replace to="/" />} />
+      <Route exact path="/learn" element={<Learn />}>
+        <Route exact path="courses" element={<Courses />} >
+          <Route exact path=":courseid" element={<CourseId />} />
+        </Route> 
+        <Route exact path="bundles" element={<Bundles />} />
+      </Route>
+      <Route exact path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
+
+
+function Home()
+{
+  return(
+    <div>
+      <h1>This is Home Component</h1>
+    </div>
+  )
+}
+
+function Learn()
+{
+  return(
+    <div>
+      <h1>This is Learn Component</h1>
+      <h4>All Courses are listed here</h4>
+      <Link className='btn btn-success' to="/learn/courses">Course</Link> ||| {"  "}
+      <Link className='btn btn-primary' to="/learn/bundles">Bundle</Link>
+      
+      <Outlet />
+
+    </div>
+  )
+}
+
+
+function Courses()
+{
+  const courseList = ["React","Angular","Vue","Nodejs"];
+  const courseName = courseList[Math.floor(Math.random()*courseList.length)];
+  return(
+    <div>
+      <h1>Course List</h1>
+      <h4>Course Card</h4>
+
+      <NavLink style={({isActive}) => { return {backgroundColor: isActive ? "Pink" : "yellow",textDecoration : "none"}}} to={`/learn/courses/${courseName}`}>{courseName}</NavLink>
+    {"  "} ||| {"  "}
+      <NavLink style={({isActive}) => { return {backgroundColor: isActive ? "Pink" : "yellow",textDecoration : "none"}}} to={`/learn/courses/test`}>Test</NavLink>
+
+
+
+
+      <Outlet />
+    </div>
+  )
+}
+
+
+function Bundles()
+{
+  return(
+    <div>
+      <h1>Bundle List</h1>
+      <h4>Bundle Card</h4>
+    </div>
+  )
+}
+
+
+
+function CourseId()
+{
+  const navigate = useNavigate();
+  const {courseid} = useParams();
+  return(
+    <div>
+      <h1>Course Id is : {courseid}</h1>
+      <button onClick={() => navigate("/dashboard",{state:"399"})} className='btn btn-warning'>Price</button>
+      <Link to="/dashboard" state={"DJANGO"}>Test</Link>
+    </div>
+  )
+}
+
+
+
+function Dashboard()
+{
+  const price = useLocation();
+  return(
+    <div>
+      <h1>course price : {price.state}</h1>
+    </div>
+  )
+}
+
+
+
+
+
+
 reportWebVitals();
